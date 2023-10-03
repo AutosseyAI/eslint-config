1b0ba40ad22cf4f2ca69a6454aed39dbba09dba4
Connor White <connor.bcw@gmail.com>
2023-10-03 07:03:43 -0500

    feat: separate node eslint config

---
+++ source/node.ts
@@ -0,0 +1,20 @@
+import type { Linter } from "eslint";
+import eslintOverride from "./overrides/eslint/index.js";
+import jsonOverride from "./overrides/json/index.js";
+import nodeOverride from "./overrides/node/index.js";
+import typescriptOverride from "./overrides/typescript/index.js";
+
+const config: Linter.Config = {
+  env: {
+    browser: false,
+    node: true
+  },
+  overrides: [
+    eslintOverride,
+    typescriptOverride,
+    nodeOverride,
+    jsonOverride
+  ]
+};
+
+module.exports = config;
--- source/overrides/node/index.ts
+++ source/overrides/node/index.ts
@@ -1,16 +1,13 @@
 import type { Linter } from "eslint";
-import importRules from "./eslint-plugin-import.js";
-import eslintRules from "./eslint.js";
+import nodeRules from "./eslint-plugin-n.js";
 
 const override: Linter.ConfigOverride = {
   files: ["*.ts", "*.tsx", "*.js", "*.jsx"],
   extends: [
-    "eslint:recommended",
-    "plugin:import/recommended"
+    "plugin:n/recommended"
   ],
   rules: {
-    ...eslintRules,
-    ...importRules
+    ...nodeRules
   },
   parser: "@typescript-eslint/parser",
   // https://typescript-eslint.io/packages/parser/#configuration
@@ -24,7 +21,7 @@ const override: Linter.ConfigOverride = {
     warnOnUnsupportedTypeScriptVersion: false
   },
   plugins: [
-    "import"
+    "n"
   ],
   settings: {
     "import/parsers": {
